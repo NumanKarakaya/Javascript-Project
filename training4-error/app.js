431da068aef2fa6ef151c258849486a19b45a521
Numan Karakaya <numan1820@hotmail.com>
2019-06-27 20:00:36 +0300

    beşinci çalışmamdır

---
+++ training4-error/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path=require('path');
+
+
+
+
+
+app.get('/', function (req,res) {
+
+   
+    res.sendFile(path.join(__dirname + '/index.html'));
+    
+  })
+
+.listen(8080);
+ 
